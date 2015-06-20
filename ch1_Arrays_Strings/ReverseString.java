3967279d2adad683c6cbd33e97524cb322002008
Peng <eamonpeng91@gmail.com>
2015-06-20 16:30:48 -0400

    Update and rename ReverseString.c to ReverseString.java

--- ch1_Arrays_Strings/ReverseString.java
+++ ch1_Arrays_Strings/ReverseString.java
@@ -2,6 +2,4 @@
 1.2
 Java strings are not terminated with a null characters as in C or C++. Although java strings uses internally the char array but there is no terminating null in that. String class provides a method called length to know the number of characters in the string.
 */
-void reverse(char *str) {
-  
-}
+
