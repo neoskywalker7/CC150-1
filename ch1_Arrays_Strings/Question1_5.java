9d0e92f52ec62ae79a92455cf553f7e43d0d09e0
Peng <eamonpeng91@gmail.com>
2015-06-22 13:15:39 -0400

    Update Question1_5.java

--- ch1_Arrays_Strings/Question1_5.java
+++ ch1_Arrays_Strings/Question1_5.java
@@ -1,3 +1,104 @@
 /*
 1.5
 */
+public class StrCompressSol {
+	// O(p + k^2) time 
+	// p is the size of the original string 
+	// k is the number of character sequences
+	public static String compressBad(String str) {
+		String mystr = "";
+		char last = str.charAt(0);
+		int count = 1;
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == last) {
+				count++;
+			} else {
+				mystr += last + "" + count;
+				last = str.charAt(i);
+				count = 1;
+			}
+		}
+		return mystr + last + count;
+	}
+	// O(N) time, O(N) space
+	public static String compressBetter(String str) {
+		int size = countCompression(str);
+		if (size >= str.length()) {
+			return str;
+		}
+		
+		StringBuffer mystr = new StringBuffer();
+		char last = str.charAt(0);
+		int count = 1;
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == last) {
+				count++;
+			} else {
+				mystr.append(last);
+				mystr.append(count);
+				last = str.charAt(i);
+				count = 1;
+			}
+		}
+		mystr.append(last);
+		mystr.append(count);
+		return mystr.toString();
+	}
+	
+	private static int countCompression(String str) {
+		if (str == null || str.isEmpty()) return 0;
+		char last = str.charAt(0);
+		int size = 0;
+		int count = 1;
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == last) {
+				count++;
+			} else {
+				last = str.charAt(i);
+				size += 1 + String.valueOf(count).length();
+				count = 1;
+			}
+		}
+		size += 1 + String.valueOf(count).length();
+		return size;
+	}
+	// O(N) time, O(N) space
+	public static String compressAlternate(String str) {
+		int size = countCompression(str);
+		if (size >= str.length()) {
+			return str;
+		}
+		
+		char[] array = new char[size];
+		int index = 0;
+		char last = str.charAt(0);
+		int count = 1;
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == last) {
+				count++;
+			} else {
+				index = setChar(array, last, index, count);
+				last = str.charAt(i);
+				count = 1;
+			}
+		}
+		index = setChar(array, last, index, count);
+		return String.valueOf(array);
+	}
+	
+	private static int setChar(char[] array, char c, int index, int count) {
+		array[index] = c;
+		index++;
+		char[] cnt = String.valueOf(count).toCharArray();
+		for (char x : cnt) {
+			array[index] = x;
+			index++;
+		}
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		String str = "aabca";
+		System.out.print(compressAlternate(str));
+	}
+}
