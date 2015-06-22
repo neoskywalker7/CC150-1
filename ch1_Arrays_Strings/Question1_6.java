83b6c6eba987b085905632a807baa1d1e03bc289
Peng <eamonpeng91@gmail.com>
2015-06-22 14:19:56 -0400

    Update Question1_6.java

--- ch1_Arrays_Strings/Question1_6.java
+++ ch1_Arrays_Strings/Question1_6.java
@@ -6,6 +6,7 @@ In other contexts there is a difference between ++i and i++, but not for loops.
 */
 public class RotateSol {
 	// O(N^2) time
+	// matrix[i][j] i: rows, j: columns
 	public static void rotate(int[][] matrix, int n) {
 		for (int layer = 0; layer < n / 2; layer++) {
 			int first = layer;
@@ -28,10 +29,47 @@ public class RotateSol {
 	}
 	
 	public static void main(String[] args) {
+		
 		int[][] matrix = AssortedMethods.randomMatrix(4, 4, 0, 3);
 		AssortedMethods.printMatrix(matrix);
 		rotate(matrix, 4);
 		System.out.println();
 		AssortedMethods.printMatrix(matrix);
+
+		System.out.println();
+		int[][] test = {{4, 2, 1, 3}, {1, 3, 2, 4}, {3, 1, 2, 4}, {2, 4, 1, 3}};
+		for (int i = 0; i < test.length; i++) {
+			for (int j = 0; j < test[i].length; j++) {
+				if (test[i][j] < 10 && test[i][j] > -10) {
+					System.out.print(" ");
+				}
+				if (test[i][j] < 100 && test[i][j] > -100) {
+					System.out.print(" ");
+				}
+				if (test[i][j] >= 0) {
+					System.out.print(" ");
+				}
+				System.out.print(" " + test[i][j]);
+			}
+		    System.out.println();
+		}
+		rotate(test, 4);
+		System.out.println();
+		for (int i = 0; i < test.length; i++) {
+			for (int j = 0; j < test[i].length; j++) {
+				if (test[i][j] < 10 && test[i][j] > -10) {
+					System.out.print(" ");
+				}
+				if (test[i][j] < 100 && test[i][j] > -100) {
+					System.out.print(" ");
+				}
+				if (test[i][j] >= 0) {
+					System.out.print(" ");
+				}
+				System.out.print(" " + test[i][j]);
+			}
+			System.out.println();
+		}
+		
 	}
 }
