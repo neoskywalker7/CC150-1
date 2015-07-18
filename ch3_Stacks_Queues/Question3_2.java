cca6dfe20d142ed48d8177a80c5ccdab7422fddc
Peng <eamonpeng91@gmail.com>
2015-07-18 17:22:48 -0400

    Update Question3_2.java

--- ch3_Stacks_Queues/Question3_2.java
+++ ch3_Stacks_Queues/Question3_2.java
@@ -1,3 +1,52 @@
 /*
 3.2
 */
+/*
+outputs:
+40, 
+52, 
+76, 
+15, 
+10, 
+25, 
+27, 
+54, 
+42, 
+54, 
+37, 
+16, 
+47, 
+56, 
+87, 
+
+Popped 87, 87
+New min is 10, 10
+Popped 56, 56
+New min is 10, 10
+Popped 47, 47
+New min is 10, 10
+Popped 16, 16
+New min is 10, 10
+Popped 37, 37
+New min is 10, 10
+Popped 54, 54
+New min is 10, 10
+Popped 42, 42
+New min is 10, 10
+Popped 54, 54
+New min is 10, 10
+Popped 27, 27
+New min is 10, 10
+Popped 25, 25
+New min is 10, 10
+Popped 10, 10
+New min is 15, 15
+Popped 15, 15
+New min is 40, 40
+Popped 76, 76
+New min is 40, 40
+Popped 52, 52
+New min is 40, 40
+Popped 40, 40
+New min is 2147483647, 2147483647
+*/
