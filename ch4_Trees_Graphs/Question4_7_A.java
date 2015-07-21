4ee516fa2deda66095fa14a0d256086ca9b9b7ed
Peng <eamonpeng91@gmail.com>
2015-07-21 00:15:36 -0400

    Update and rename Question4_7.java to Question4_7_A.java

---
+++ ch4_Trees_Graphs/Question4_7_A.java
@@ -0,0 +1,115 @@
+/*
+4.7
+*/
+public class Question4_7_A {
+	public static Project[] findBuildOrder(String[] projects, String[][] dependencies) {
+		Graph graph = buildGraph(projects, dependencies);
+		return orderProjects(graph.getNodes());
+	}
+	
+	public static Graph buildGraph(String[] projects, String[][] dependencies) {
+		Graph graph = new Graph();
+		for (String project : projects) {
+			graph.getOrCreateNode(project);
+		}
+		
+		for (String[] dependency : dependencies) {
+			String first = dependency[0];
+			String second = dependency[1];
+			graph.addEdge(first, second);
+		}
+		
+		return graph;
+	}
+	
+	public static Project[] orderProjects(ArrayList<Project> projects) {
+		Project[] order = new Project[projects.size()];
+		int endOfList = addNonDependent(order, projects, 0);
+		int toBeProcessed = 0;
+		while (toBeProcessed < order.length) {
+			Project current = order[toBeProcessed];
+			
+			if (current == null) {
+				return null;
+			}
+			
+			ArrayList<Project> children = current.getChildren();
+			for (Project child : children) {
+				child.decrementDependencies();
+			}
+			
+			endOfList = addNonDependent(order, children, endOfList);
+			toBeProcessed++;
+		}
+		
+		return order;
+	}
+	
+	public static int addNonDependent(Project[] order, ArrayList<Project> projects, int offset) {
+		for (Project project : projects) {
+			if (project.getNumberDependencies() == 0) {
+				order[offset] = project;
+				offset++;
+			}
+		}
+		return offset;
+	}
+	
+	public static class Graph {
+		private ArrayList<Project> nodes = new ArrayList<Project>();
+		private HashMap<String, Project> map = new HashMap<String, Project>();
+		
+		public Project getOrCreateNode(String name) {
+			if (!map.containsKey(name)) {
+				Project node = new Project(name);
+				nodes.add(node);
+				map.put(name, node);
+			}
+			
+			return map.get(name);
+		}
+		
+		public void addEdge(String startName, String endName) {
+			Project start = getOrCreateNode(startName);
+			Project end = getOrCreateNode(endName);
+			start.addNeighbor(end);
+		}
+		
+		public ArrayList<Project> getNodes() { return nodes; }
+	}
+	
+	public static class Project {
+		private ArrayList<Project> children = new ArrayList<Project>();
+		private HashMap<String, Project> map = new HashMap<String, Project>();
+		private String name;
+		private int dependencies = 0;
+		
+		public Project(String n) {
+			name = n;
+		}
+		
+		public void addNeighbor(Project node) {
+			if (!map.containsKey(node.getName())) {
+				children.add(node);
+				node.incrementDependencies();
+			}
+		}
+		
+		public void incrementDependencies() {
+			dependencies++;
+		}
+		public void decrementDependencies() {
+			dependencies--;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public ArrayList<Project> getChildren() {
+			return children;
+		}
+		public int getNumberDependencies() {
+			return dependencies;
+		}
+	}
+}
