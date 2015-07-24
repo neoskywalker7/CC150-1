de2696a06997bd43a8d7ad49636cf01a946ceae0
Peng <eamonpeng91@gmail.com>
2015-07-24 02:28:44 -0400

    Update Question4_9.java

--- ch4_Trees_Graphs/Question4_9.java
+++ ch4_Trees_Graphs/Question4_9.java
@@ -1,3 +1,69 @@
 /*
 4.9
 */
+public class Question4_9 {
+	public static ArrayList<LinkedList<Integer>> allSequences(TreeNode node) {
+		ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+		
+		if (node == null) {
+			result.add(new LinkedList<Integer>());
+			return result;
+		}
+		
+		LinkedList<Integer> prefix = new LinkedList<Integer>();
+		prefix.add(node.val);
+		
+		ArrayList<LinkedList<Integer>> leftSeq = allSequences(node.left);
+		ArrayList<LinkedList<Integer>> rightSeq = allSequences(node.right);
+		
+		for (LinkedList<Integer> left : leftSeq) {
+			for (LinkedList<Integer> right : rightSeq) {
+				ArrayList<LinkedList<Integer>> weaved = new ArrayList<LinkedList<Integer>>();
+				weaveLists(left, right, weaved, prefix);
+				result.addAll(weaved);
+			}
+		}
+		return result;
+	}
+	
+	public static void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second,
+			ArrayList<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
+		if (first.size() == 0 || second.size() == 0) {
+			LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+			result.addAll(first);
+			result.addAll(second);
+			results.add(result);
+			return;
+		}
+		
+		int headFirst = first.removeFirst();
+		prefix.addLast(headFirst);
+		weaveLists(first, second, results, prefix);
+		prefix.removeLast();
+		first.addFirst(headFirst);
+		
+		int headSecond = second.removeFirst();
+		prefix.addLast(headSecond);
+		weaveLists(first, second, results, prefix);
+		prefix.removeLast();
+		second.addFirst(headSecond);
+	}
+	
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		public TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	public static void main(String[] args) {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		n2.left = n1;
+		n2.right = n3;
+		System.out.print(allSequences(n2).toString());
+	}
+}
